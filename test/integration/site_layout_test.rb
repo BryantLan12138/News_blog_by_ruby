c95344fe33ce048e838fb43d979d466d066ffc10
Daniel Dominique <s3745409@student.rmit.edu.au>
2020-05-03 16:19:34 +1000

    Updated test cases

--- test/integration/site_layout_test.rb
+++ test/integration/site_layout_test.rb
@@ -1,7 +1,11 @@
 require 'test_helper'
 
 class SiteLayoutTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+
+  test "layout links" do
+    get root_path
+    assert_template 'static_pages/home'
+    assert_select "a[href=?]", root_path, count: 1
+    assert_select "a[href=?]", news_path
+  end
 end
