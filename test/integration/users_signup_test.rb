1513183504d72ee92d45a60ae84d37672b15029a
Daniel Dominique <s3745409@student.rmit.edu.au>
2020-05-09 22:16:00 +1000

    Added n+1 testing for logout and remember function

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -1,30 +1,31 @@
 require 'test_helper'
 
 class UsersSignupTest < ActionDispatch::IntegrationTest
-
+ 
   test "invalid signup information" do
     get signup_path
     assert_no_difference 'User.count' do
-      post users_path, params: { user: { name:  "",
-                                         email: "user@invalid",
-                                         password:              "foo",
-                                         password_confirmation: "bar" } }
-    end
-    assert_template 'users/new'
-    assert_select 'div#error_explanation'
-    assert_select 'div.field_with_errors'
-  end
-
+      post users_path, params: {user: {
+                                  name: "",
+                                  email: "user@invalid",
+                                  password: "foo",
+                                  password_confirmation: "bar"
+                                }}
+                          end
+      # assert_template 'users/new'
+  end 
   test "valid signup information" do
     get signup_path
     assert_difference 'User.count', 1 do
-      post users_path, params: { user: { name:  "Example User",
-                                         email: "user@example.com",
-                                         password:              "password",
-                                         password_confirmation: "password" } }
-    end
-    follow_redirect!
-    assert_template 'users/show'
-    assert is_logged_in?
-  end
+      post users_path, params: {user: {
+                                  name: "Helloa",
+                                  email: "Helloa@Helloa.com",
+                                  password: "Helloa",
+                                  password_confirmation: "Helloa"
+                                }}
+                          end
+      follow_redirect!
+      assert_template 'users/show'
+      assert is_logged_in?
+  end 
 end
