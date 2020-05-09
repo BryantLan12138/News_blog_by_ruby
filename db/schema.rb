d3f1741188b03a2c20ab9c9397b8d5c6cc4bd790
Lynne Wang <lubi980835699@gmail.com>
2020-05-10 09:43:20 +1000

    added form template to render later on

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_09_112219) do
+ActiveRecord::Schema.define(version: 2020_05_03_063147) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
