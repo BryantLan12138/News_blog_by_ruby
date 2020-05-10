875adf19b0fbb9da4a6eb910185648de2597735e
Lynne Wang <lubi980835699@gmail.com>
2020-05-10 11:16:00 +1000

    edit before logged in

--- test/integration/users_edit_test.rb
+++ test/integration/users_edit_test.rb
@@ -5,6 +5,7 @@ class UsersEditTest < ActionDispatch::IntegrationTest
   end
 
   test "successful edit" do
+    log_in_as(@user)
     get edit_user_path(@user) 
     assert_template 'users/edit' 
     name = "Foo Bar"
@@ -18,5 +19,17 @@ class UsersEditTest < ActionDispatch::IntegrationTest
     @user.reload
     assert_equal name, @user.name 
     assert_equal email, @user.email
-  end 
+  end
+  
+  
+  test "unsuccessful edit" do 
+    log_in_as(@user)
+    get edit_user_path(@user)
+    assert_template 'users/edit'
+    patch user_path(@user), params: { user: { name: "",
+                                              email: "foo@invalid",
+                                              password:             "foo",
+                                              password_confirmation: "bar" } }
+    assert_template 'users/edit'
+  end
 end
