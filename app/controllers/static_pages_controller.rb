221654a26255b1b085c044fc5a29878d633dcdba
Lynne Wang <lubi980835699@gmail.com>
2020-05-11 14:25:22 +1000

    index

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -2,6 +2,8 @@ class StaticPagesController < ApplicationController
   def home
   end
 
+  
+
   def news
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,13 +2,16 @@ Rails.application.routes.draw do
   root  'static_pages#home'
   get   'sessions/new'
   get   'users/new'
+  
+
   get   '/news',    to: 'static_pages#news'
-  get'/signup', to: 'users#new'
-  post'/signup', to: 'users#create'
-  get '/login', to: 'sessions#new'
-  post '/login', to: 'sessions#create'
+  get   '/signup', to: 'users#new'
+  post  '/signup', to: 'users#create'
+  get   '/login', to: 'sessions#new'
+  post  '/login', to: 'sessions#create'
   delete '/logout', to: 'sessions#destroy'
-  get '/', to: 'users#index'
+
+
   
   resources :users
 end
