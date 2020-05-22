788084ea843099af2880ad93a845e226a7f293c2
Daniel Dominique <danmeldominique@gmail.com>
2020-05-22 15:31:44 +1000

    Updated tests

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -35,20 +35,18 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
     assert_not @other_user.admin?
   end
 
-  # test "should redirect edit when logged in as wrong user" do
-  #   log_in_as(@other_user) 
-  #   get edit_user_path(@user) 
-  #   assert flash.empty?
-  #   assert_redirected_to root_url
-  # end
+  test "should redirect edit when logged in as wrong user" do
+    log_in_as(@other_user) 
+    get edit_user_path(@user) 
+    assert_redirected_to login_path
+  end
 
-  # test "should redirect update when logged in as wrong user" do 
-  #   log_in_as(@other_user)
-  #   patch user_path(@user), params: { user: { name: @user.name,
-  #                                             email: @user.email } }
-  #   assert flash.empty?
-  #   assert_redirected_to root_url
-  # end
+  test "should redirect update when logged in as wrong user" do 
+    log_in_as(@other_user)
+    patch user_path(@user), params: { user: { name: @user.name,
+                                              email: @user.email } }
+    assert_redirected_to login_path
+  end
 
 end
 
