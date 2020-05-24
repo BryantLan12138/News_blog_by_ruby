4cda6872249b4c8cc5ee9766e3fae38c41a4c15e
Daniel Dominique <danmeldominique@gmail.com>
2020-05-24 13:16:59 +1000

    Troubleshoot db schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_23_095331) do
+ActiveRecord::Schema.define(version: 2020_05_23_092856) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -47,14 +47,14 @@ ActiveRecord::Schema.define(version: 2020_05_23_095331) do
   create_table "users", force: :cascade do |t|
     t.string "name"
     t.string "email"
-    t.integer "phone"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "password_digest"
-    t.string "avatar"
     t.string "remember_digest"
+    t.string "phone"
     t.string "city"
     t.string "card"
+    t.boolean "admin", default: false
     t.string "reset_digest"
     t.datetime "reset_sent_at"
     t.datetime "lasttime"
